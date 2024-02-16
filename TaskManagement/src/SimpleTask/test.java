bfb1f2e05f7ab53b96300be58db585b5e404fb26
ByteCoderRohith <142328932+ByteCoderRohith@users.noreply.github.com>
2024-02-16 22:33:16 +0530

    Update test.java

--- TaskManagement/src/SimpleTask/test.java
+++ TaskManagement/src/SimpleTask/test.java
@@ -1,16 +1,1 @@
 package SimpleTask;
-
-interface BaseI{ void method();}
-
-public class test {
-	void method() {
-		System.out.println("Inside test method");
-	}
-
-}
-class B extends test implements BaseI{
-	
-	public static void main(String[] args) {
-		
-	}
-}
